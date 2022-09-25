75f9c240bade2ae53463355ab07ec8ecf71cb342
notauserx <notauserx@users.noreply.github.com>
2022-09-25 23:02:15 +0600

    add test for randompixeldatagenerator

---
+++ PixelSort.Domain.Tests/HelperTests/RandomPixelDataGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PixelSort.Domain.Tests.HelperTests
+{
+    public class RandomPixelDataGeneratorTests
+    {
+        [Theory]
+        [InlineData(10)]
+        [InlineData(15)]
+        [InlineData(1)]
+        [InlineData(0)]
+        public void Test_Random_Pixel_Size(int count)
+        {
+            var generator = new RandomPixelDataGenerator();
+
+            Assert.Equal(count, generator.GenerateRandomPixelData(count).Length);
+        }
+    }
+}
