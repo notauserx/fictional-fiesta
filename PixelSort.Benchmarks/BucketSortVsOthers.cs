79d5ec434d152477d3dfdbf504190670b752c7be
notauserx <notauserx@users.noreply.github.com>
2022-09-25 21:43:32 +0600

    add benchmark project

---
+++ PixelSort.Benchmarks/BucketSortVsOthers.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using PixelSort.Domain;
+using System.Collections.Generic;
+
+namespace PixelSort.Benchmarks
+{
+    public class BucketSortVsOthers
+    {
+
+        public IEnumerable<Pixel[]> Data()
+        {
+            yield return
+                new RandomPixelDataGenerator().GenerateRandomPixelData(400 * 300);
+            yield return
+                new RandomPixelDataGenerator().GenerateRandomPixelData(1024 * 768);
+            yield return
+                new RandomPixelDataGenerator().GenerateRandomPixelData(1920 * 1080);
+
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Data))]
+        public Pixel[] BucketSort(Pixel[] pixels)
+        {
+
+            var sorter = new BucketSortPixelSorter();
+            return sorter.GetSortedPixels(pixels);
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Data))]
+        public Pixel[] ArraySort(Pixel[] pixels)
+        {
+            var sorter = new ArraySortPixelSorter();
+            return sorter.GetSortedPixels(pixels);
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Data))]
+        public Pixel[] LinqSort(Pixel[] pixels)
+        {
+            var sorter = new LinqOrderByPixelSorter();
+            return sorter.GetSortedPixels(pixels);
+        }
+    }
+}
---
+++ PixelSort.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace PixelSort.Benchmarks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BucketSortVsOthers>();
+        }
+    }
+}
