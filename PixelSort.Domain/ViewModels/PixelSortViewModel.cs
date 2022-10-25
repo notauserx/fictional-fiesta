01f56cb7f7dafa78142f06892b1271523aa0c3e0
notauserx <notauserx@users.noreply.github.com>
2022-10-25 23:38:45 +0600

    add comments to viewmodel class

--- PixelSort.Domain/ViewModels/PixelSortViewModel.cs
+++ PixelSort.Domain/ViewModels/PixelSortViewModel.cs
@@ -5,6 +5,9 @@ using System.Windows.Media.Imaging;
 
 namespace PixelSort.Domain
 {
+    /// <summary>
+    /// Viewmodel class for MainWindow
+    /// </summary>
     public class PixelSortViewModel : ViewModelBase
     {
         private readonly TaskFactory uiTaskFactory;
@@ -31,6 +34,9 @@ namespace PixelSort.Domain
             IsSortButtonEnabled = true;
         }
 
+        /// <summary>
+        /// The image source for the pixel image
+        /// </summary>
         public BitmapSource WriteableBitmap 
         { 
             get => bitmapService.WriteableBitmap; 
@@ -39,6 +45,11 @@ namespace PixelSort.Domain
         public bool ArePixelsSorted() => pixelService.ArePixelsSorted();
         
         private bool _isRandomButtonEnabled;
+
+        /// <summary>
+        /// Represents wheather the random pixel button is enabled
+        /// Random pixel button is disabled when the application is generating random pixels.
+        /// </summary>
         public bool IsRandomButtonEnabled
         {
             get => _isRandomButtonEnabled;
@@ -50,6 +61,11 @@ namespace PixelSort.Domain
         }
 
         private bool _isSortButtonEnabled;
+
+        /// <summary>
+        /// Represents wheather the sort pixel button is enabled.
+        /// Sort pixel button is diabled when the application is sorting the pixels.
+        /// </summary>
         public bool IsSortButtonEnabled
         {
             get => _isSortButtonEnabled;
@@ -61,6 +77,12 @@ namespace PixelSort.Domain
         }
 
         private Visibility _imageVisibility;
+        
+        /// <summary>
+        /// The visibility of the image on main window.
+        /// Initially the image visibility is set to hidden.
+        /// When back buffer is updated with random pixels, Visibility is set to visible.
+        /// </summary>
         public Visibility ImageVisibility
         {
             get => _imageVisibility;
@@ -71,6 +93,10 @@ namespace PixelSort.Domain
             }
         }
 
+        /// <summary>
+        /// Updates the back buffer of the writable bitmap with randomly generated pixel data
+        /// and sets the image visibility to visible if the image is hidden.
+        /// </summary>
         public void UpdateBackBufferWithRandomPixelData()
         {
             IsRandomButtonEnabled = false;
@@ -97,6 +123,11 @@ namespace PixelSort.Domain
             });
         }
 
+
+        /// <summary>
+        /// Updates the back buffer of the writable bitmap with sorted pixels
+        /// Does not update the back buffer if the pixels are empty or when pixels are already sorted.
+        /// </summary>
         public void UpdateBackBufferWithSortedPixelData()
         {
             if (pixelService.ArePixelsEmpty() || pixelService.ArePixelsSorted())
